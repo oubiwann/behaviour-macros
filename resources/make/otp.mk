84351d9fae2f1e736d5fe3046c5332f8a2b0e31d
Duncan McGreggor <duncan@cogitat.io>
2014-06-06 11:37:15 -0700

    Fixed up make includes.

--- resources/make/otp.mk
+++ resources/make/otp.mk
@@ -1,3 +1,5 @@
+NODENAME=$(shell echo "behaviour-macros"|sed -e 's/-//g')
+
 dev:
 	@echo "Running OTP app in the foreground ..."
 	@ERL_LIBS=$(ERL_LIBS) PATH=$(SCRIPT_PATH) lfe \
@@ -5,17 +7,34 @@ dev:
 
 run: dev
 
+dev-named:
+	@echo "Running OTP app in the foreground ..."
+	@ERL_LIBS=$(ERL_LIBS) PATH=$(SCRIPT_PATH) lfe \
+	-sname repl@${HOST} -setcookie `cat ~/.erlang.cookie` \
+	-eval "application:start('behaviour-macros')"
+
+run-named: dev-named
+
 prod:
 	@echo "Running OTP app in the background ..."
 	@ERL_LIBS=$(ERL_LIBS) PATH=$(SCRIPT_PATH) lfe \
-    -eval "application:start('behaviour-macros')" \
-	-name behaviour-macros@$${HOSTNAME} -setcookie `cat ~/.erlang.cookie` \
+	-sname ${NODENAME}@${HOST} -setcookie `cat ~/.erlang.cookie` \
+	-eval "application:start('behaviour-macros')" \
 	-noshell -detached
 
 daemon: prod
 
 stop:
+	@echo "Stopping OTP app ..."
 	@ERL_LIBS=$(ERL_LIBS) PATH=$(SCRIPT_PATH) lfe \
-	-eval "rpc:call('behaviour-macros@$${HOSTNAME}', init, stop, [])" \
-	-name controller@$${HOSTNAME} -setcookie `cat ~/.erlang.cookie` \
+	-sname controller@${HOST} -setcookie `cat ~/.erlang.cookie` \
+	-eval "rpc:call('${NODENAME}@${HOST}', init, stop, [])" \
+	-noshell -s erlang halt
+
+list-nodes:
+	@echo "Getting list of running OTP nodes ..."
+	@echo
+	@ERL_LIBS=$(ERL_LIBS) PATH=$(SCRIPT_PATH) lfe \
+	-sname controller@${HOST} -setcookie `cat ~/.erlang.cookie` \
+	-eval 'io:format("~p~n",[element(2,net_adm:names())]).' \
 	-noshell -s erlang halt
